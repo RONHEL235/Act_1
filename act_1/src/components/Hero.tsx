ef8c6bb1c9fcaccd407e592cb1df4c48181a4aaa
Ronee <137155067+RONHEL235@users.noreply.github.com>
2026-01-01 12:58:39 +0200

    Add Hero section with TypeScript

---
+++ act_1/src/components/Hero.tsx
@@ -0,0 +1,12 @@
+function Hero() {
+  return (
+    <section id="home">
+      <h1>Hi, I’m Ronee 👋</h1>
+      <p>
+        I’m a Philosopher, Writer and Software Engineering student passionate about building.
+      </p>
+    </section>
+  )
+}
+
+export default Hero
